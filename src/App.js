9df22580a672e83df9214ae44c99b77a7b64b2c9
Anabel <villalobos1998@gmail.com>
2022-07-06 21:42:53 -0700

    section 4.79

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './categories.styles.scss'
+import Directory from './components/directory/directory.component'
 
 
 const App = () => {
@@ -33,18 +33,8 @@ const App = () => {
   
 
   return (
-    <div className="categories-container">
-      {categories.map(({id, title, imageUrl}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }}/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
+    <div className="">
+      <Directory categories={categories}/>
     </div>
   );
 }
