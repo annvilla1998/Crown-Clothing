03f76ffaa5aa11b4f31f54ce9fa5d667e6de8f9d
Anabel <villalobos1998@gmail.com>
2022-07-12 11:01:09 -0700

    completed section 6

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, 
     signInWithPopup, 
     GoogleAuthProvider,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
  } from 'firebase/auth';
 
 import {
@@ -73,4 +74,10 @@ export const createAuthUserWithEmailAndPassword = async(email, password) => {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthUserWithEmailAndPassword = async(email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password)
 }
