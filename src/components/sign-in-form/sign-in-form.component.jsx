45cf49f81853ad387aa506abf8c1863211a1f9d8
Anabel <villalobos1998@gmail.com>
2022-08-09 22:26:31 -0700

    fixed button styles

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
 import './sign-in-form.styles.scss';
-import Button from '../button/button.component'
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component'
 import { signInWithGooglePopup, 
     signInAuthUserWithEmailAndPassword
  } from '../../utils/firebase/firebase.utils'
@@ -77,7 +77,7 @@ const SignInForm = () => {
                 <div className='buttons-container'>
                     <Button type="submit">Sign In</Button>
                     <Button type="button" 
-                    buttonType='google' 
+                    buttonType={BUTTON_TYPE_CLASSES.google}
                     onClick={signInWithGoogle}
                     >Google sign in</Button>
                 </div>
