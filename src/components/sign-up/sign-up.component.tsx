1ba893d351b43ffbc0a8cbd70d1ff43912a63ee5
Anabel <villalobos1998@gmail.com>
2022-12-20 07:13:56 -0800

    typed out signup form

--- src/components/sign-up/sign-up.component.tsx
+++ src/components/sign-up/sign-up.component.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
+import { AuthError, AuthErrorCodes } from 'firebase/auth';
 import { useDispatch } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import { SignUpContainer } from './sign-up-form.styles.js';
 import Button from '../button/button.component';
 import { signUpStart } from '../../store/user/user.action';
+
 
 const defaultFormFields = {
     displayName: '',
@@ -23,7 +25,7 @@ const SignUpForm = () => {
         setFormFields(defaultFormFields)
     }
     
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         
         if(password !== confirmPassword) {
@@ -35,18 +37,19 @@ const SignUpForm = () => {
             dispatch(signUpStart(email, password, displayName));
             resetFormFields();
         }catch(error) {
-            if(error.code === 'auth/email-already-in-use') {
+            if((error as AuthError).code === AuthErrorCodes.EMAIL_EXISTS) {
                 alert('Cannot create user email already in use.')
             }
-            console.log('user creation encountered an error',error.message)
+            console.log('user creation encountered an error',error)
         }
     }
 
-    const handleChange = (event) => {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
 
         setFormFields({ ...formFields, [name]: value });
     }
+
 
     return (
         <SignUpContainer>
